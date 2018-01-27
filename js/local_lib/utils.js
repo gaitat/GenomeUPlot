739b3bbed50de3f44302d3d2f28bf0d0437abb73
Athanasios Gaitatzes <athanasios.gaitatzes@mayo.edu>
2018-01-26 18:38:45 -0600

    inline console.save

--- js/local_lib/utils.js
+++ js/local_lib/utils.js
@@ -2,8 +2,6 @@
  * @author gaitat / Athanasios Gaitatzes (Saki)
  */
 
-import '../../vendor/console-save';
-
 "use strict";
 
 export function assert ( condition, message )
@@ -22,6 +20,29 @@ export function assert ( condition, message )
 export function ofType ( object )
 {
 	return ({}).toString.call(object).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+}
+
+console.save = function(data, filename) {
+  if(!data) {
+    console.error('Console.save: No data');
+    return;
+  }
+
+  if(!filename) filename = 'console.json';
+
+  if(typeof data === "object"){
+    data = JSON.stringify(data, undefined, 4)
+  }
+
+  var blob = new Blob([data], {type: 'text/json'}),
+    e    = document.createEvent('MouseEvents'),
+    a    = document.createElement('a');
+
+  a.download = filename;
+  a.href = window.URL.createObjectURL(blob);
+  a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':');
+  e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  a.dispatchEvent(e);
 }
 
 export function saveJSONtoCSV( json, fileName )
